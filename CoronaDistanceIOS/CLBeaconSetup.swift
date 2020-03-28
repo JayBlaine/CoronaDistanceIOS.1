947829e095c466023cc05fd77a3bbcd8a65d5d1c
JayBlaine <jbrags42@gmail.com>
2020-03-28 12:47:45 -0500

    Removed view for CLBeaconSetup

--- CoronaDistanceIOS/CLBeaconSetup.swift
+++ CoronaDistanceIOS/CLBeaconSetup.swift
@@ -62,23 +62,3 @@ func updateDistance(_ distance: CLProximity) {
         }
     }
 }
-
-
-
-
-struct CLBeaconSetup: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-
-    //PREVIEW BELOW
-
- 
-
-struct CLBeaconSetup_Previews: PreviewProvider {
-    static var previews: some View {
-        CLBeaconSetup()
-    }
-}
