b01e6e5694ad2fbbcf275860681ad7d9d34448aa
JayBlaine <jbrags42@gmail.com>
2020-03-28 18:36:44 -0500

    Stored XMLParser into CLBeacon

--- CoronaDistanceIOS/CLBeaconSetup.swift
+++ CoronaDistanceIOS/CLBeaconSetup.swift
@@ -1,74 +1,138 @@
-//
-//  CLBeaconSetup.swift
-//  CoronaDistanceIOS
-//
-//  Created by Jarrod Ragsdale on 3/28/20.
-//  Copyright © 2020 Group. All rights reserved.
-//
+//Enter the class information here {
+import Foundation
 
-import SwiftUI
-
-/*
-func viewDidLoad() {
-    //super.viewDidLoad()
-
-    locationManager = CLLocationManager()
-    //locationManager.delegate = self
-    locationManager.requestAlwaysAuthorization()
-}
-
-func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-    if status == .authorizedAlways {
-        if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
-            if CLLocationManager.isRangingAvailable() {
-                startScanning()
+struct RSSItem
+    {
+    var title: String
+    var description: String
+    var pubdate: String
+    }
+    
+// download xml from a server, parse xml to foundation object
+class FeedParser: NSObject, XMLParserDelegate
+    {
+    public var rssItems: [RSSItem]= []
+    public var currentKey = ""
+    public var currentTitle: String = ""
+    {
+        didSet
+        {
+        currentTitle = currentTitle.trimmingCharacters(in: characterSet.whitespacesAndNewlines)
+        }
+    }
+    public currentDescription: String = ""
+        {
+        didSet
+        {
+        currentDescription = currentDescription.trimmingCharacters(in: characterSet.whitespacesAndNewlines)
+        }
+    }
+    public var currentPubDate: String = ""
+        {
+        didSet
+        {
+        currentPubDate = currentPubDate.trimmingCharacters(in: characterSet.whitespacesAndNewlines)
+        }
+    }
+    public var parserCompletion Handler: (([RSSItem]) -> Void)?
+    }
+    
+    func parseFeed(url: String, completionHandler: (([RSSItem]) -> Void?
+        {
+        self.parserCompletionHandler = completionHandler
+        let request = URLRequest(url: URL(string: url)!)
+        let urlSession = urlSession.shared
+        let task = urlSession,dataTask(with: request) { (data, response, error) in
+            guard let data = data
+            else
+                {
+                if let error = error
+                    {
+                    print(error.localizedDescription)
+                    }
+                    return
+                }
+                //this will parse the xml data
+                let parser = XMLParser(data: data)
+                parser.delegate = self
+                parser.parse()
+            }
+            task.resume()
+        }
+        
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributesDict: [String : String] = [ : ])
+        {
+        currentElement = elementName
+        if currentElement == "item"
+            {
+            currentTitle = ""
+            currentDescription = ""
+            CurrentPubDate = ""
             }
         }
-    }
-}
-
-func startScanning() {
-    let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-    let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
-
-    locationManager.startMonitoring(for: beaconRegion)
-    locationManager.startRangingBeacons(in: beaconRegion)
-}
-
-func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-    if beacons.count > 0 {
-        updateDistance(beacons[0].proximity)
-    } else {
-        updateDistance(.unknown)
-    }
-	/*
-	let content = UNMutableNotificationContent()
-		content.title = "Forget Me Not"
-		content.body = "Are you forgetting something?"
-		content.sound = .default()
-    
-	let request = UNNotificationRequest(identifier: "ForgetMeNot", content: content, trigger: nil)
-	UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-	*/
-}
-
-func updateDistance(_ distance: CLProximity) {
-        switch distance {
-        case .unknown:
-            return
-
-        case .far:
-            return
-
-        case .near:
-            iBeaconNear = true
-
-        case .immediate:
-            iBeaconNear = true
-        @unknown default:
-            return
+        
+    func parser(_ parser: XMLParser, foundCharacters temp: String)
+        {
+        switch currentElement
+            {
+            case "title": currentTitle += temp
+            
+            case "description": currentDescription += temp
+            
+            case "pubdate": currentPubDate += temp
+            default: break
+            }
         }
-    
+        
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
+        {
+        if elementName == "item"
+            {
+            let rssItem = RSSItem(title: currentTitle, description: currentDescription, pubDate: currentPubDate)
+            self.rssItems.append(rssItem)
+            }
+        }
+        
+    func parserDidEndDocument(_ parser: XMLParser)
+        {
+        parserCompletionHandler?(rssItems)
+        }
+        
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error)
+        {
+        print(parseError.localizedDescription)
+        }
 }
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
