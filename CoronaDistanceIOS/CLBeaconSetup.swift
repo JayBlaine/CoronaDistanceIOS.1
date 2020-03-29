91a995b470077354dd297b713dcbf2d3d14dce3f
patrickmv01 <60357351+patrickmv01@users.noreply.github.com>
2020-03-28 20:21:33 -0500

    Update CLBeaconSetup.swift

--- CoronaDistanceIOS/CLBeaconSetup.swift
+++ CoronaDistanceIOS/CLBeaconSetup.swift
@@ -76,6 +76,7 @@ class FeedParser: NSObject, XMLParserDelegate
         
     func parser(_ parser: XMLParser, foundCharacters temp: String)
         {
+		/*
         switch currentElement
             {
             case "title": currentTitle += temp
@@ -85,6 +86,21 @@ class FeedParser: NSObject, XMLParserDelegate
             case "pubDate": currentPubDate += temp
             default: break
             }
+		*/
+		if currentElement == "title"
+			{
+				currentTitle += temp
+			}
+		else if currentElement == "description"
+			{ 
+				currentElement += temp
+			}
+		else if currentElement == "pubDate"
+			{
+				currentElement += temp
+			}
+		else
+		break
         }
         
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
