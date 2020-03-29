378ab9d40ff8b889ef8fa685b8d2dbe4a32fcaca
JayBlaine <jbrags42@gmail.com>
2020-03-29 01:07:16 -0500

    Added JSON Dependancies/News File

---
+++ CoronaDistanceIOS/newsFile.swift
@@ -0,0 +1,13 @@
+//
+//  newsFile.swift
+//  CoronaDistanceIOS
+//
+//  Created by Jarrod Ragsdale on 3/29/20.
+//  Copyright © 2020 Group. All rights reserved.
+//
+
+import Foundation
+
+
+
+//a7e759e71fa0436b89b9d4c353a8f1f0
